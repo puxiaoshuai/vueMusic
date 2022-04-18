3f7422ec18a7285df638c4988841458238a4fd37
puxiaoshuai <1372553910@qq.om>
2022-04-18 20:13:25 +0800

    a

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,7 +2,7 @@
   <div class="home-wrapper">
     <van-nav-bar fixed title="首页" />
     <div style="margin-top: 46px">
-      <p>测试</p>
+      <p>测试1</p>
       <router-link :to="{ path: '/detail/1233', query: { name: 'puxioashuai' } }">去详情</router-link>
       <van-button type="primary" @click="add">点击+1</van-button>
       <p>{{ $store.state.number }}</p>
